000fd01dd0e58a31ac037a29621a7610c19142ba
JustinMoscoso <116162941+JustinMoscoso@users.noreply.github.com>
2025-06-27 00:58:04 +0800

    Add project files.

---
+++ SchoolHunt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SchoolHunt.MvcApplication" Language="C#" %>
---
+++ SchoolHunt/Models/School.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolHunt.Models
+{
+    public class School
+    {
+        public int SchoolId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Type { get; set; }
+        public string level { get; set; }
+        public decimal TuitionFee { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+    }
+}
